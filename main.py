0f3667f466d0eea92c680474b64fd35ccf891f75
Haze272 <studiorain27@gmail.com>
2023-03-06 23:58:45 +0300

    Заполнение карты

--- main.py
+++ main.py
@@ -2,5 +2,14 @@ from utils import *
 
 
 if __name__ == '__main__':
-    map_ =
+    map_ = read_station_information('./Stations.txt')
+    vector_ = read_information('./InfoVelocity.txt')
+    connections_ = read_cost_table('./Time.txt')
 
+    map_.add_connection(connections_)
+    map_.add_velocity(vector_)
+
+    path1 = Path(1)
+    path1.add_route(2)
+
+    print(path1.route)
